3842ad1f96254c735102f1a319558023cc228c62
Debayan Ghosh <149896687+Debayan-Ghosh2005@users.noreply.github.com>
2025-06-15 19:50:41 +0530

    Update update-weather.py

--- update-weather.py
+++ update-weather.py
@@ -1,42 +1,29 @@
 import requests
-from datetime import datetime
+import os
 
-API_KEY = os.environ.get("WEATHER_API_KEY")
-LOCATION = "Kolkata"
+API_KEY = os.getenv("WEATHER_API_KEY")
+CITY = "Kolkata"
+URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}&aqi=no"
 
-def get_weather():
-    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={LOCATION}&aqi=no"
-    response = requests.get(url)
-    return response.json()
+response = requests.get(URL)
+data = response.json()
 
-def update_readme(weather_data):
-    with open("README.md", "r", encoding="utf-8") as f:
-        content = f.read()
+temp_c = data['current']['temp_c']
+condition = data['current']['condition']['text']
+icon = data['current']['condition']['icon']
 
-    icon_url = f"https:{weather_data['current']['condition']['icon']}"
-    condition = weather_data['current']['condition']['text']
-    temp_c = weather_data['current']['temp_c']
-    date_str = datetime.now().strftime("%d/%m/%Y")
+weather_block = f"![icon](https:{icon}) **{CITY}**: {temp_c}°C, {condition}"
 
-    weather_html = f"""
-<h3 align="center">Today's Weather</h3>
-<div align="center">
-  <p>Kolkata, India - {date_str}</p>
-  <img src="{icon_url}" />
-  <p>{condition}, {temp_c}°C</p>
-</div>
-"""
+with open("README.md", "r", encoding="utf-8") as f:
+    content = f.read()
 
-    # Replace between these two tags
-    start_tag = "<!-- WEATHER-START -->"
-    end_tag = "<!-- WEATHER-END -->"
-    start = content.find(start_tag)
-    end = content.find(end_tag)
+start_tag = "<!-- WEATHER-START -->"
+end_tag = "<!-- WEATHER-END -->"
 
-    if start != -1 and end != -1:
-        new_content = content[:start + len(start_tag)] + weather_html + content[end:]
-        with open("README.md", "w", encoding="utf-8") as f:
-            f.write(new_content)
+start = content.find(start_tag)
+end = content.find(end_tag)
 
-weather_data = get_weather()
-update_readme(weather_data)
+if start != -1 and end != -1:
+    new_content = content[:start + len(start_tag)] + "\n" + weather_block + "\n" + content[end:]
+    with open("README.md", "w", encoding="utf-8") as f:
+        f.write(new_content)
